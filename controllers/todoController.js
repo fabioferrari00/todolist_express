74a9cf688d2c669ec1f8f48cd583a6ca9b9c4e21
fabioferrari00 <fabioferrari.ff00@gmail.com>
2026-04-13 10:21:38 +0200

    controllers e routers di update e delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -17,3 +17,28 @@ exports.createTodo = (req, res) => {
     res.json({ id: result.insertId, title, status: 'todo' })
   })
 }
+
+//UPDATE
+exports.updateTodo = (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  db.query(
+    'UPDATE todos SET status = ? WHERE id = ?',
+    [status, id],
+    (err) => {
+      if (err) return res.status(500).json(err);
+      res.json({ message: 'Todo aggiornata' });
+    }
+  );
+};
+
+//DELETE
+exports.deleteTodo = (req, res) => {
+  const { id } = req.params;
+
+  db.query('DELETE FROM todos WHERE id = ?', [id], (err) => {
+    if (err) return res.status(500).json(err);
+    res.json({ message: 'Todo eliminata' });
+  });
+};
--- routes/todos.js
+++ routes/todos.js
@@ -4,3 +4,7 @@ const todoController = require('../controllers/todoController');
 
 router.get('/', todoController.getTodos);
 router.post('/', todoController.createTodo);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
